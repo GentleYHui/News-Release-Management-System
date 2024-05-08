2774218e5dac5ededa1863e6b2a029d380a7b690
YHui <140957964+GentleYHui@users.noreply.github.com>
2024-05-08 19:30:57 +0800

    Update AliOssUtil.java

--- src/main/java/com/yhui/utils/AliOssUtil.java
+++ src/main/java/com/yhui/utils/AliOssUtil.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 
 public class AliOssUtil {
     // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-    private static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    private static final String ENDPOINT = "";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
 //        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
     private static final String ACCESS_KEY_ID = "";
